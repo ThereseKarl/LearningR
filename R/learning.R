c47c70af1582fa0aa2a881eb12ed5ea77e3e944b
Therese Karlsson <therese.e.karlsson@hotmail.com>
2023-01-16 16:05:46 +0100

    add inittial R file

---
+++ R/learning.R
@@ -0,0 +1,31 @@
+10
+20
+30
+
+# R basics ----------------------------------------------------------------
+
+weight_kilos <- 100
+weight_kilos <- 10
+weight_kilos <- 20
+
+weight_kilos
+weight_kilos
+
+colnames(airquality)
+
+str(airquality)
+
+summary(airquality)
+
+2 + 2
+
+3 + 2
+
+1 + 1
+
+3 + 3
+
+# Packages ----------------------------------------------------------------
+
+library(tidyverse)
+
